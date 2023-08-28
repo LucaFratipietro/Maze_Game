b33cb39888fd80b61918922d220a6ab232a1383b
LucaFratipietro <luca.fratipietro@dawsoncollege.qc.ca>
2023-08-28 11:01:08 -0400

    Fixed folder structure to allow project to run

---
+++ Maze/Map.cs
@@ -0,0 +1,32 @@
+﻿namespace Maze
+{
+    public class Map : IMap
+    {
+        public MapVector Goal => throw new NotImplementedException();
+
+        public int Height => throw new NotImplementedException();
+
+        public bool IsGameFinished => throw new NotImplementedException();
+
+        public Block[,] MapGrid => throw new NotImplementedException();
+
+        public IPlayer Player => throw new NotImplementedException();
+
+        public int Width => throw new NotImplementedException();
+
+        public void CreateMap()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateMap(int width, int height)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveDirectionMap(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Maze/MapVector.cs
@@ -0,0 +1,21 @@
+﻿namespace Maze
+{
+    public class MapVector : IMapVector
+    {
+        public bool IsValid => throw new NotImplementedException();
+
+        public int X => throw new NotImplementedException();
+
+        public int Y => throw new NotImplementedException();
+
+        public bool InsideBoundary(int width, int height)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Magnitude()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
