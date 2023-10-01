875d9541f2886a353c532a1afc8959ee792ca17f
LucaFratipietro <luca.fratipietro@dawsoncollege.qc.ca>
2023-10-01 12:22:21 -0400

    Add loading of map from file before monogame initialized

--- MonoGame/MazeGame.cs
+++ MonoGame/MazeGame.cs
@@ -2,6 +2,8 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.IO;
+using System.Windows.Forms;
 using MFile = MazeFromFile.MazeFromFile;
 
 namespace MonoGame;
@@ -24,8 +26,32 @@ public class MazeGame : Game
     protected override void Initialize()
     {
         //player must pick a valid map .txt from their file directory to load
-        var filePath = "";
+
+        /*var filePath = "";
+        using(OpenFileDialog openFileDialog = new OpenFileDialog())
+        {
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "txt files (*.txt)|*.txt|All files(*.*)|*.*";
+            openFileDialog.FilterIndex = 2;
+            openFileDialog.RestoreDirectory = true;
+            
+            if(openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                //gets the path for the map you want to load
+                filePath = openFileDialog.FileName;
+            }
+        }*/
+
+        string sCurrentDirectory = System.AppDomain.CurrentDomain.BaseDirectory;
+        string sFile = System.IO.Path.Combine(sCurrentDirectory, $@"..\..\..\..\map9x7.txt");
+        string sFilePath = Path.GetFullPath(sFile);
+
+        //Once map is chosen, create map object for the game
+        IMapProvider mapPro = new MazeFromFile.MazeFromFile(sFilePath);
+        _map = new Map(mapPro);
+        _map.CreateMap();
         
+        //when Map is set, start initializing game
         base.Initialize();
     }
 
@@ -41,7 +67,7 @@ public class MazeGame : Game
 
     protected override void Update(GameTime gameTime)
     {
-        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == Microsoft.Xna.Framework.Input.ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Microsoft.Xna.Framework.Input.Keys.Escape))
             Exit();
 
         // TODO: Add your update logic here
