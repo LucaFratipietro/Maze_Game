aba186bac1264d512e0e49535b4b7adb5d89194f
LucaFratipietro <luca.fratipietro@dawsoncollege.qc.ca>
2023-11-27 11:16:07 -0500

    Added Timeit and Writetofile

--- BenchMarking/Benchmarking.cs
+++ BenchMarking/Benchmarking.cs
@@ -3,24 +3,28 @@ using MazeHuntKillSpace;
 
 namespace performance
 {
-    public class Benchmarking
-    {
-
-        public static void Main(string[] args)
-        {
-            //get both IMaps using the factory methods
-            Map Huntmap = new Map(IMapFactory.MapFactory(null, "Hunt"));
-            Map Recmap = new Map(IMapFactory.MapFactory(null, "Recursion"));
-        }
-
-        public TimeSpan Timeit(Action bench)
+        public class Benchmarking
         {
 
-        }
+            public static void Main(string[] args)
+            {
+                //get both IMaps using the factory methods
+                Map Huntmap = new Map(IMapFactory.MapFactory(null, "Hunt"));
+                Map Recmap = new Map(IMapFactory.MapFactory(null, "Recursion"));
 
-        public void WriteToFile(string path, string data)
-        {
+                int runs = 0;
+                while (runs < 1000) {
+                TimeSpan span = Timeit(() => { Huntmap.CreateMap(9, 9) };
+            }
 
-        }
+            private TimeSpan Timeit(Action bench)
+            {
+
+            }
+
+            private void WriteToFile(string path, string data)
+            {
+
+            }
     }
-}
+    }
