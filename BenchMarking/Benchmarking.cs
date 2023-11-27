8e2c206035352582a89e4cb3cab6c8c7e08416d6
LucaFratipietro <luca.fratipietro@dawsoncollege.qc.ca>
2023-11-27 11:05:53 -0500

    Added benchmarking console app skeleton

---
+++ BenchMarking/Benchmarking.cs
@@ -0,0 +1,26 @@
+﻿using Maze;
+using MazeHuntKillSpace;
+
+namespace performance
+{
+    public class Benchmarking
+    {
+
+        public static void Main(string[] args)
+        {
+            //get both IMaps using the factory methods
+            Map Huntmap = new Map(IMapFactory.MapFactory(null, "Hunt"));
+            Map Recmap = new Map(IMapFactory.MapFactory(null, "Recursion"));
+        }
+
+        public TimeSpan Timeit(Action bench)
+        {
+
+        }
+
+        public void WriteToFile(string path, string data)
+        {
+
+        }
+    }
+}
