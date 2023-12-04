deec5aa7391c21a5266ee68d6f49ec23384ab798
lucafratipietro <luca.fratipietro@dawsoncollege.qc.ca>
2023-12-04 12:14:02 -0500

    Average the time to make 50 hunt maps

--- BenchMarking/Benchmarking.cs
+++ BenchMarking/Benchmarking.cs
@@ -1,30 +1,49 @@
-﻿using Maze;
+﻿using System.Diagnostics;
+using Maze;
 using MazeHuntKillSpace;
 
 namespace performance
 {
-        public class Benchmarking
+    public class Benchmarking
+    {
+        public static void Main(string[] args)
         {
 
-            public static void Main(string[] args)
-            {
-                //get both IMaps using the factory methods
-                Map Huntmap = new Map(IMapFactory.MapFactory(null, "Hunt"));
-                Map Recmap = new Map(IMapFactory.MapFactory(null, "Recursion"));
+            //get both IMaps using the factory methods
+            IMapProvider Recmap = IMapFactory.MapFactory(null, "Recursion");
 
+            //first test the hunt algo 
+            Console.WriteLine("Hunt Algo Tests starting... \n");
+            for (int i = 0; i < 50; i+= 2)
+            {
+                Console.WriteLine($"{i + 5} by {i + 5} tests");
                 int runs = 0;
-                while (runs < 1000) {
-                TimeSpan span = Timeit(() => { Huntmap.CreateMap(9, 9) };
+                List<double> results = new List<double>();
+                while (runs < 5)
+                {
+                    IMapProvider Huntmap = IMapFactory.MapFactory(null, "Hunt");
+                    TimeSpan span = Timeit(() => { Huntmap.CreateMap(i+5, i+5); });
+                    results.Add(span.TotalMilliseconds);
+                    runs++;
+                }
+                Console.WriteLine("Average timespan: " + results.Average() + "\n");
             }
+        }
 
-            private TimeSpan Timeit(Action bench)
-            {
+        public static TimeSpan Timeit(Action bench)
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+            bench.Invoke();
+            timer.Stop();
+            TimeSpan timeTaken = timer.Elapsed;
+            return timeTaken;
+        }
 
-            }
+        private void WriteToFile(string path, string data)
+        {
 
-            private void WriteToFile(string path, string data)
-            {
+        }
 
-            }
     }
-    }
+}
